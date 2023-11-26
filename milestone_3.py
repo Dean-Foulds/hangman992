40db7bbbd21a9ff78aeb259062d5cd0a810bfc56
Dean-Foulds <deanfoulds@gmail.com>
2023-11-26 18:21:28 +0000

    refactored milestone 3

--- milestone_3.py
+++ milestone_3.py
@@ -1,30 +1,44 @@
 import random
 
-secret_word = "apple"  # Assume the secret word is "apple" for demonstration purposes.
+def choose_secret_word():
+    """Choose a random secret word for the game."""
+    words = ["apple", "banana", "orange", "grapes", "watermelon"]
+    return random.choice(words)
 
-def check_guess(guess):
+def check_guess(secret_word, guess):
     """Check if the guessed letter is in the word."""
-    guess = guess.lower()  # Step 2: Convert the guess into lower case.
+    guess = guess.lower()
 
-    # Step 3: Move the code to check if the guess is in the word into this function block.
     if guess in secret_word:
-        print(f"Good guess! {guess} is in the word.")
+        print(f"Good guess! '{guess}' is in the word.")
     else:
-        print(f"Sorry, {guess} is not in the word. Try again.")
+        print(f"Sorry, '{guess}' is not in the word. Try again.")
 
-def ask_for_input():
-    """Ask the user to guess a letter and validate the input."""
+def ask_for_guess():
+    """Prompt the user to guess a letter and validate the input."""
     while True:
         guess = input("Guess a letter: ")
 
         if len(guess) == 1 and guess.isalpha():
             break
         else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
+            print("Invalid input. Please enter a single alphabetical character.")
 
-    # Step 3: Call the check_guess function to check if the guess is in the word.
-    check_guess(guess)
+    return guess
 
-# Step 4: Call the ask_for_input function to test your code.
-ask_for_input()
+def play_game():
+    """Main function to play the guessing game."""
+    secret_word = choose_secret_word()
+
+    while True:
+        guess = ask_for_guess()
+        check_guess(secret_word, guess)
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            break
+
+if __name__ == "__main__":
+    play_game()
+
 
